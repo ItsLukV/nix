8d8e37f4254384e9578b1adaf7201b7899b61d4b
Lukas <lukasvranic@gmail.com>
2025-08-07 20:10:00 +0200

    added steam and some cleanup

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -82,7 +82,6 @@ in {
       bindm = [
         "${mod}, mouse:272, movewindow"
         "${mod}, mouse:273, resizewindow"
-        "${mod} ALT, mouse:272, resizewindow"
       ];
 
       input = {
