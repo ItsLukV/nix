d8f6fbc10fb803ab16c196c36e3c696045709121
Lukas <lukasvranic@gmail.com>
2025-08-08 18:31:07 +0200

    updated packages, but hyprland-plugin is kinda broken

--- nixos/default.nix
+++ nixos/default.nix
@@ -65,7 +65,6 @@
   services.printing.enable = true;
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
