c1bab972365d81280a097bb21c03e89511eff7f2
Lukas <lukasvranic@gmail.com>
2025-09-11 07:04:35 +0000

    changed color of promt

--- home/lukas/bash.nix
+++ home/lukas/bash.nix
@@ -8,5 +8,12 @@
       rebuild = "sudo nixos-rebuild switch --flake ${flakePath}#$(hostname)";
 			hms = "home-manager switch --flake ${flakePath}#$(hostname)";
     };
+
+    initExtra = ''
+      parse_git_branch() {
+        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+      }
+      export PS1="\[\033[32m\][\u@\h:\w]\[\033[36m\]\$(parse_git_branch) \[\033[00m\]$ "
+    '';
   };
 }
