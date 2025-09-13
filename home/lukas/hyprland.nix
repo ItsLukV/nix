3cd3d9bf648dfabff5611ced0565ae0d1aed4430
Lukas <lukasvranic@gmail.com>
2025-09-13 17:43:04 +0200

    added window groups

--- home/lukas/hyprland.nix
+++ home/lukas/hyprland.nix
@@ -24,14 +24,25 @@ in {
       x11.enable = true;
     };
   };
+  
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    
+    # Correct plugin reference
     plugins = [
+      (inputs.hyprland.packages.${pkgs.system}.hyprland + "/lib/libhyprgroup.so")
       inputs.split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
     ];
 
     settings = {
+      # Monitor configuration should be at the top level, not in settings.monitor
+      monitor = [
+        "HDMI-A-1,preferred,auto,1"
+        "DP-1,preferred,auto-left,1"
+        "DP-2,preferred,auto-right,1"
+      ];
+
       general = {
         gaps_in = 10;
         gaps_out = 10;
@@ -44,59 +55,63 @@ in {
         ];
       };
 
-      settings.monitor = [
-        "HDMI-A-1,preferred,auto,1"
-        "DP-1,preferred,auto-left,1"
-        "DP-2,preferred,auto-right,1"
-      ];
-
       env = [
         "XCURSOR_THEME,Adwaita"
         "XCURSOR_SIZE,24"
       ];
 
       exec-once = ["${startupScript}/bin/start"];
-      #      monitor = [",preferred,auto,1"];
-      /*
-      monitor = [
-        "HDMI-A-1,preferred,auto,1"
-        "DP-1,preferred,auto-left,1"
-        "DP-2,preferred,auto-right,1"
-      ];
-      */
+      
+      group = {
+        # Insert new windows after the currently active window
+        insert_after_current = true;
+        # Focus the group when a new window is added to it
+        focus_removed_window = true;
+      };
+
+      # Use consistent variable name for modifier key
+      "$mainMod" = "SUPER";
+
       bind = [
-        "${mod}, Q, exec, ${pkgs.firefox}/bin/firefox"
-        "${mod}_SHIFT, C, killactive"
-        "${mod}, Return, exec, ${terminal}"
-        "${mod}, Space, togglefloating,"
-        "${mod}, S, exec, ${pkgs.wofi}/bin/wofi --show drun"
-        "${mod}, M, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        "$mainMod, Q, exec, ${pkgs.firefox}/bin/firefox"
+        "$mainMod SHIFT, C, killactive"
+        "$mainMod, Return, exec, ${terminal}"
+        "$mainMod, Space, togglefloating,"
+        "$mainMod, S, exec, ${pkgs.wofi}/bin/wofi --show drun"
+        "$mainMod, M, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
 
-        "${mod}, 1, split-workspace, 1"
-        "${mod}, 2, split-workspace, 2"
-        "${mod}, 3, split-workspace, 3"
-        "${mod}, 4, split-workspace, 4"
-        "${mod}, 5, split-workspace, 5"
-        "${mod}, 6, split-workspace, 6"
-        "${mod}, 7, split-workspace, 7"
-        "${mod}, 8, split-workspace, 8"
-        "${mod}, 9, split-workspace, 9"
+        # Grouping keybinds - these are correct
+        "$mainMod, G, togglegroup"
+        "$mainMod SHIFT, G, lockactivegroup"
+        "$mainMod, Tab, changegroupactive"
+        "$mainMod, apostrophe, moveoutofgroup"
 
-        "${mod} SHIFT, 1, split-movetoworkspace, 1"
-        "${mod} SHIFT, 2, split-movetoworkspace, 2"
-        "${mod} SHIFT, 3, split-movetoworkspace, 3"
-        "${mod} SHIFT, 4, split-movetoworkspace, 4"
-        "${mod} SHIFT, 5, split-movetoworkspace, 5"
-        "${mod} SHIFT, 6, split-movetoworkspace, 6"
-        "${mod} SHIFT, 7, split-movetoworkspace, 7"
-        "${mod} SHIFT, 8, split-movetoworkspace, 8"
-        "${mod} SHIFT, 9, split-movetoworkspace, 9"
+        # Split workspace binds
+        "$mainMod, 1, split-workspace, 1"
+        "$mainMod, 2, split-workspace, 2"
+        "$mainMod, 3, split-workspace, 3"
+        "$mainMod, 4, split-workspace, 4"
+        "$mainMod, 5, split-workspace, 5"
+        "$mainMod, 6, split-workspace, 6"
+        "$mainMod, 7, split-workspace, 7"
+        "$mainMod, 8, split-workspace, 8"
+        "$mainMod, 9, split-workspace, 9"
+
+        "$mainMod SHIFT, 1, split-movetoworkspace, 1"
+        "$mainMod SHIFT, 2, split-movetoworkspace, 2"
+        "$mainMod SHIFT, 3, split-movetoworkspace, 3"
+        "$mainMod SHIFT, 4, split-movetoworkspace, 4"
+        "$mainMod SHIFT, 5, split-movetoworkspace, 5"
+        "$mainMod SHIFT, 6, split-movetoworkspace, 6"
+        "$mainMod SHIFT, 7, split-movetoworkspace, 7"
+        "$mainMod SHIFT, 8, split-movetoworkspace, 8"
+        "$mainMod SHIFT, 9, split-movetoworkspace, 9"
       ];
 
       bindm = [
-        "${mod}, mouse:272, movewindow"
-        "${mod}, mouse:273, resizewindow"
-        "${mod} ALT, mouse:272, resizewindow"
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+        "$mainMod ALT, mouse:272, resizewindow"
       ];
 
       input = {
@@ -105,8 +120,7 @@ in {
         sensitivity = 1;
         touchpad = {
           disable_while_typing = true;
-          natural_scroll = true; # Enable "natural" scrolling (like macOS)
-          #natural_scroll = false; # Disable (traditional scrolling)
+          natural_scroll = true;
         };
       };
 
@@ -118,6 +132,7 @@ in {
         animate_mouse_windowdragging = false;
       };
     };
+    
     extraConfig = ''
       device {
         name=logitech-usb-receiver
